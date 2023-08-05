fe7e3d54429d0556575d306fa81e2b0a8f523b52
Ramon Flowers <rocketorangemen@gmail.com>
2023-08-05 17:29:28 -0500

    javascript progress - getting button to display selected team on console

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
-var name = window.prompt("What is your name?");
-document.write("Hello " + name + ", how are you today?");
+// var name = window.prompt("What is your name?");
+// document.write("Hello " + name + ", how are you today?");
+
+var button = document.querySelector('#submit');
+var teams = document.querySelectorAll('input[name="favorite-team"]');
+var it = document.querySelector('#it');
+// var favteam = document.querySelector(radio.name);
+// console.log(teams.values);
+// console.log(radio.name);
+
+button.addEventListener('click', () => {
+    var selectedTeam;
+    for (var favteam of teams) {
+        if(teams.check) {
+            selectedTeam = favteam.value;
+            console.log(selectedTeam);
+        }
+    }
+});
+
+
