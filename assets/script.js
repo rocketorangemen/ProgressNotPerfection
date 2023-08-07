109586bc2fbcfbf2ee6a6412b801444fe879daf2
Ramon Flowers <rocketorangemen@gmail.com>
2023-08-06 22:36:31 -0500

    Working on javascript - unable to select individual teams - progress not perfection

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,23 @@
 // document.write("Hello " + name + ", how are you today?");
 
 var button = document.querySelector('#submit');
-var teams = document.querySelectorAll('input[name="favorite-team"]');
+// var teams = document.querySelectorALL('input[type="radio"]');
+var pack = document.querySelector('#favorite-team')
 var it = document.querySelector('#it');
 // var favteam = document.querySelector(radio.name);
 // console.log(teams.values);
 // console.log(radio.name);
 
-button.addEventListener('click', () => {
-    var selectedTeam;
-    for (var favteam of teams) {
-        if(teams.check) {
-            selectedTeam = favteam.value;
-            console.log(selectedTeam);
-        }
-    }
-});
+// button.addEventListener('click', () => {
+//     let selectedTeam;
+//     for (const favteam of teams) {
+//         if(teams.check) {
+//             selectedTeam = favteam.value;
+//             console.log(selectedTeam);
+//             break;
+//         }
+//     }
+// });
 
+console.log(pack.value);
 
