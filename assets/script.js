3713850ce509ff78a1906d60e0e1f82b84be0e6f
Ramon Flowers <rocketorangemen@gmail.com>
2023-08-14 12:22:16 -0500

    javascript restart

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,2 @@
-// var name = window.prompt("What is your name?");
-// document.write("Hello " + name + ", how are you today?");
-
-var button = document.querySelector('#submit');
-// var teams = document.querySelectorALL('input[type="radio"]');
-var pack = document.querySelector('#favorite-team')
-var it = document.querySelector('#it');
-// var favteam = document.querySelector(radio.name);
-// console.log(teams.values);
-// console.log(radio.name);
-
-// button.addEventListener('click', () => {
-//     let selectedTeam;
-//     for (const favteam of teams) {
-//         if(teams.check) {
-//             selectedTeam = favteam.value;
-//             console.log(selectedTeam);
-//             break;
-//         }
-//     }
-// });
-
-console.log(pack.value);
-
+var favteam = document.getElementsByName('favorite-team');
+console.log(favteam);
