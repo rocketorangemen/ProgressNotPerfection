590f937202f94474d7a20960b84d880733743cd3
Ramon Flowers <rocketorangemen@gmail.com>
2023-08-19 21:17:57 -0500

    script progress on submit button

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
 var favteam = document.getElementsByName('favorite-team');
 var selectedteam = favteam.values;
+var submit = document.getElementById('submit');
+
 console.log(favteam);
 console.log(selectedteam);
+console.log(submit);
