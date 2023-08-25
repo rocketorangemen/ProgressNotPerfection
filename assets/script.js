a177f6eb0a96511c9a98dab83aa2800e7565ceed
Ramon Flowers <rocketorangemen@gmail.com>
2023-08-24 23:59:06 -0500

    script var added

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var favteam = document.getElementsByName('favorite-team');
 var selectedteam = favteam.values;
 var submit = document.getElementById('submit');
+var userinput = selectedteam.value;
 
 // Looking to see variables in console
 // console.log(favteam);
