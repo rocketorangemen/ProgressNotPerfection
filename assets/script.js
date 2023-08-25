d2a09bf0761cc7106744122d7cebd82c3c53b538
Ramon Flowers <rocketorangemen@gmail.com>
2023-08-24 23:58:15 -0500

    script updates

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var favteam = document.getElementsByName('favorite-team');
 var selectedteam = favteam.values;
 var submit = document.getElementById('submit');
 
-console.log(favteam);
-console.log(selectedteam);
-console.log(submit);
+// Looking to see variables in console
+// console.log(favteam);
+// console.log(selectedteam);
+// console.log(submit);
