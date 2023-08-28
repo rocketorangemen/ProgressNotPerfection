bc1aa37c03f5610ad120724ec47d7e74df864176
Ramon Flowers <rocketorangemen@gmail.com>
2023-08-27 22:36:24 -0500

    next step loop through nodelist to get all team name values in console

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 var favteam = document.getElementsByName('favorite-team');
-var selectedteam = favteam.values;
+var selectedteam = favteam.values.toString;
 var submit = document.getElementById('submit');
 var userinput = selectedteam.value;
 
 // Looking to see variables in console
-// console.log(favteam);
-// console.log(selectedteam);
-// console.log(submit);
+console.log(favteam);
+console.log(selectedteam);
+console.log(userinput);
