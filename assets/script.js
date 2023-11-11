fe05012aadcec61f47da49d161380bb30b8fe4ad
Ramon Flowers <rocketorangemen@gmail.com>
2023-11-11 15:04:00 -0600

    changing titles in html

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,9 @@ var selectedteam = favteam.values.toString;
 var submit = document.getElementById('submit');
 var userinput = selectedteam.value;
 
+// favteam.forEach()
+
 // Looking to see variables in console
 console.log(favteam);
-console.log(selectedteam);
-console.log(userinput);
+// console.log(selectedteam);
+// console.log(userinput);
