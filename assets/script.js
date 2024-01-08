152f4292505af107f6c5966acdd63a75b0f870b4
Ramon Flowers <rocketorangemen@gmail.com>
2024-01-08 00:39:56 -0600

    script.js progresss toward goal of collecting team names

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,13 @@ var favteam = document.getElementsByName('favorite-team');
 var selectedteam = favteam.values.toString;
 var submit = document.getElementById('submit');
 var userinput = selectedteam.value;
-
-// favteam.forEach()
-
-// Looking to see variables in console
 console.log(favteam);
+
+for (key in favteam) {
+    console.log(key);
+    console.log(favteam[key])
+}
+// Looking to see variables in console
 // console.log(selectedteam);
 // console.log(userinput);
 
