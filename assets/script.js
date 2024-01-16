2c2dc2463d44b57db74b26e8cd7e93f76745f5d5
Ramon Flowers <rocketorangemen@gmail.com>
2024-01-16 16:48:18 -0600

    script cannot read eventListener - working on it

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,6 @@
-// const favteam = document.getElementsByName('favorite-team');
-// const selectedteam = favteam.values.toString;
-// const submit = document.getElementById('submit');
-// const userinput = selectedteam.value;
-const radioButtons = document.querySelectorAll('label[name="favorite-team"]');
-console.log(radioButtons);
-const btn = document.getElementById('submit');
-console.log(btn.type);
-// console.log(favteam);
-
-btn.addEventListener('click', () => {
+const btn = document.getElementById('#submit');
+const radioButtons = document.querySelectorAll('input[name="favorite-team"]');
+btn.addEventListener("click",  ()  =>  {
     let selectedTeam;
     for (const radioButton of radioButtons) {
         if (radioButton.checked) {
@@ -16,16 +8,5 @@ btn.addEventListener('click', () => {
             break;
         }
     }
+    console.log(selectedTeam);
 })
-
-// Work in progress on getting the name of teams put into console log
-// for (key in favteam) {
-//     console.log(key);
-//     // console.log(favteam[key])
-// };
-
-// Looking to see variables in console
-// console.log(selectedteam);
-// console.log(userinput);
-
-// defaultvalue is the property withing the array holding the team names
