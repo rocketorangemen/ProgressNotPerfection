e04779223d1d4e0e64f71ddf048e16434a66f10e
Ramon Flowers <rocketorangemen@gmail.com>
2024-01-16 14:51:20 -0600

    submit progress

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,29 @@
-var favteam = document.getElementsByName('favorite-team');
-var selectedteam = favteam.values.toString;
-var submit = document.getElementById('submit');
-var userinput = selectedteam.value;
-console.log(favteam);
+// const favteam = document.getElementsByName('favorite-team');
+// const selectedteam = favteam.values.toString;
+// const submit = document.getElementById('submit');
+// const userinput = selectedteam.value;
+const radioButtons = document.querySelectorAll('label[name="favorite-team"]');
+console.log(radioButtons);
+const btn = document.getElementById('submit');
+console.log(btn.type);
+// console.log(favteam);
 
-for (key in favteam) {
-    console.log(key);
-    // console.log(favteam[key])
-}
+btn.addEventListener('click', () => {
+    let selectedTeam;
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedTeam = radioButton.value;
+            break;
+        }
+    }
+})
+
+// Work in progress on getting the name of teams put into console log
+// for (key in favteam) {
+//     console.log(key);
+//     // console.log(favteam[key])
+// };
+
 // Looking to see variables in console
 // console.log(selectedteam);
 // console.log(userinput);
