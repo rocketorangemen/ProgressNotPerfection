e298b318cb011ac3e5df7d63ca74b8bd2f83ea78
Ramon Flowers <rocketorangemen@gmail.com>
2024-01-17 19:40:01 -0600

    working on if statements for selected team

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,26 @@
 const btn = document.getElementById('#submit');
 const radioButtons = document.getElementsByName('favorite-team');
+// const selectedTeam = radioButtons[i].value;
 
 
 function teamPick() {
 for (var i = 0, length = radioButtons.length; i < length; i++) {
     if(radioButtons[i].checked) {
         console.log(radioButtons[i].value);
-        break;
+
+        return radioButtons[i].value;
     }
 }};
+
+// console.log(selectedTeam);
+
+function jets(selectedTeam) {
+    if(selectedTeam.value = jets) {
+        console.log("green");
+        return;
+    }
+};
+
 
 
 // Submit button registry
